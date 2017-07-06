74b0577db473d233ea0cd7ae324c8c2c7d9e57d7
Max Rosado <max.a.rosado@gmail.com>
2017-07-05 20:35:48 -0400

    Splash Screen completed based on issues stated.

---
+++ NotePad/app/src/main/java/c4q/notepad/SplashActivity.java
@@ -0,0 +1,37 @@
+package c4q.notepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by maxrosado on 7/5/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//        startMainActivity();
+//        finish();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
